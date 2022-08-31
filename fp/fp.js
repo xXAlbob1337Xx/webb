18c3ee75bfb63ac258583273195da22f43359511
Albin Andersson <albin.andersson6@elev.ga.ntig.se>
2022-08-31 15:29:18 +0200

    dem är flyttade till mitten men ingen klocka

--- fp/fp.js
+++ fp/fp.js
@@ -1,11 +1,15 @@
 startTime();
 function startTime() {
 const today = new date();
-let t = today.getHours();
+let h = today.getHours();
 let m = today.getMinutes();
 let s = today.getSeconds();
 m = checktime(m);
 s = checktime(s);
 documnet.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
 setTimeout(startTime, 1000);
+
+
+function checkTime();
+if 
 }
